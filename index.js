3e7419fd7901d3ce0736174d9eff73c1f6950242
Mauro Ré <82146437+mauroree@users.noreply.github.com>
2023-04-26 19:53:17 -0300

    Implementação do mongoose

--- index.js
+++ index.js
@@ -1,76 +1,85 @@
 const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
 
-const uri = "mongodb+srv://maurojuniorpf170:dVegIlPx9npPtxUn@tarefas.fhtcpa9.mongodb.net/?retryWrites=true&w=majority";
-const dbName = 'Tarefas';
-
-MongoClient.connect(uri, (err, client) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  const db = client.db(dbName);
-  const ColecaoTarefas = db.collection('Tarefas');
-
-  app.get('/tarefas', async (req, res) => {
-    const tarefas = await ColecaoTarefas.find().toArray();
-    res.json(tarefas);
-  });
-
-  app.post('/tarefas', async (req, res) => {
-    const tarefa = {
-      nome: req.body.nome,
-      assunto: req.body.assunto,
-      notas: req.body.notas,
-      data: req.body.data,
-      horario: req.body.horario,
-    
-
-    };
-
-    const result = await ColecaoTarefas.insertOne(tarefa);
-
-    tarefa.id = result.insertedId;
-
-    res.status(201).json(tarefa);
-  });
-
-  app.put('/tarefas/:id', async (req, res) => {
-    const tarefaId = req.params.id;
-    const tarefa = await ColecaoTarefas.findOne({ _id: ObjectId(tarefaId) });
-
-    if (!tarefa) {
-      return res.status(404).send('Tarefa não encontrada.');
-    }
-
-    tarefa.nome = req.body.nome;
-    tarefa.assunto = req.body.assunto;
-    tarefa.notas = req.body.notas;
-    tarefa.data = req.body.data;
-    tarefa.horario = req.body.horario;
-   
-
-
-    await ColecaoTarefas.updateOne({ _id: ObjectId(tarefaId) }, { $set: tarefa });
-
-    res.json(tarefa);
-  });
-
-  app.delete('/tarefas/:id', async (req, res) => {
-    const tarefaId = req.params.id;
-    const result = await ColecaoTarefas.deleteOne({ _id: ObjectId(tarefaId) });
-
-    if (result.deletedCount === 0) {
-      return res.status(404).send('Tarefa não encontrada.');
-    }
-
-    res.sendStatus(204);
-  });
-
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`Servidor Rodando. Porta: ${port}`));
+mongoose.connect('mongodb+srv://maurojuniorpf170:dVegIlPx9npPtxUn@tarefas.fhtcpa9.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conectado ao MongoDB Atlas');
+}).catch((error) => {
+    console.log('Erro ao conectar ao Banco: ' + error);
 });
+
+const lembreteSchema = new mongoose.Schema({
+    nome: String,
+    assunto: String,
+    notas: String,
+    dataHora: Date,
+
+});
+
+const Lembrete = mongoose.model('lembretes', lembreteSchema);
+
+// retorna todos os lembretes
+app.get('/lembretes', async (req, res) => {
+    try {
+        const lembretes = await Lembrete.find();
+        res.send(lembretes);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// adiciona um novo lembrete
+app.post('/lembretes', async (req, res) => {
+    try {
+        const lembrete = new Lembrete(req.body);
+        await lembrete.save();
+        res.send(lembrete);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// atualiza um lembrete existente
+app.put('/lembretes/:id', async (req, res) => {
+    try {
+        const lembrete = await Lembrete.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.send(lembrete);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// deleta um lembrete existente
+app.delete('/lembretes/:id', async (req, res) => {
+    try {
+        const lembrete = await Lembrete.findByIdAndDelete(req.params.id);
+        res.send(lembrete);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// deleta todos os lembretes
+app.delete('/lembretes', async (req, res) => {
+    try {
+        await Lembrete.deleteMany();
+        res.send('Todos os lembretes foram deletados');
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
+
