e8cfd70d5b1ac03e866baf9c06f939f6ea65d6a3
Mauro Ré <82146437+mauroree@users.noreply.github.com>
2023-05-24 11:15:31 -0300

    Autenticação

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -9,33 +11,80 @@ app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://maurojuniorpf170:dVegIlPx9npPtxUn@tarefas.fhtcpa9.mongodb.net/?retryWrites=true&w=majority', {
-
+ 
     useNewUrlParser: true,
     useUnifiedTopology: true
 
 }).then(() => {
+
     console.log('Conectado ao Banco com sucesso.');
+
 }).catch((error) => {
+
     console.log('Erro ao conectar ao Banco: ' + error);
 });
 
 const lembreteSchema = new mongoose.Schema({
-    
+
     nome: String,
     assunto: String,
     notas: String,
     dataHora: Date,
-
 });
 
 const Lembrete = mongoose.model('lembretes', lembreteSchema);
 
-// retorna todos os lembretes
-app.get('/lembretes', async (req, res) => {
+const userSchema = new mongoose.Schema({
+
+    username: String,
+    password: String
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Configuração para autenticação com JWT
+const secret = 'auth-api-test';
+
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Verifica as credenciais
+    const user = await User.findOne({ username });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ error: 'Credenciais inválidas' });
+    }
+
+    // Crie o token 
+    const token = jwt.sign({ userId: user._id }, secret, { expiresIn: '1h' });
+
+    res.json({ token });
+});
+
+const authenticateToken = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ error: 'Token não fornecido' });
+    }
+
+    jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Token inválido' });
+        }
+
+        req.userId = decoded.userId;
+        next();
+    });
+};
+
+app.get('/lembretes', authenticateToken, async (req, res) => {
     try {
         const lembretes = await Lembrete.find();
         if (lembretes.length === 0) {
-            res.status(404).send({ error: 'Lembrete não encotrado' });
+            res.status(404).send({ error: 'Lembrete não encontrado' });
             return;
         }
         res.send(lembretes);
@@ -44,8 +93,7 @@ app.get('/lembretes', async (req, res) => {
     }
 });
 
-// adiciona um novo lembrete
-app.post('/lembretes', async (req, res) => {
+app.post('/lembretes', authenticateToken, async (req, res) => {
     try {
         const lembrete = new Lembrete(req.body);
         await lembrete.save();
@@ -57,17 +105,17 @@ app.post('/lembretes', async (req, res) => {
 
 
 // atualiza um lembrete existente
-app.put('/lembretes/:id', async (req, res) => {
+app.put('/lembretes/:id', authenticateToken, async (req, res) => {
     try {
         const lembrete = await Lembrete.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.status(200).send({message: "Lembrete atualizado com sucesso."});
+        res.status(200).send({ message: "Lembrete atualizado com sucesso." });
     } catch (error) {
         res.status(500).send(error);
     }
 });
 
 // deleta um lembrete existente
-app.delete('/lembretes/:id', async (req, res) => {
+app.delete('/lembretes/:id', authenticateToken, async (req, res) => {
     try {
         const lembrete = await Lembrete.findByIdAndDelete(req.params.id);
         res.status(200).send('Lembrete deletado com sucesso.');
@@ -77,7 +125,7 @@ app.delete('/lembretes/:id', async (req, res) => {
 });
 
 // deleta todos os lembretes
-app.delete('/lembretes', async (req, res) => {
+app.delete('/lembretes', authenticateToken, async (req, res) => {
     try {
         await Lembrete.deleteMany();
         res.send('Todos os lembretes foram deletados');
@@ -85,7 +133,6 @@ app.delete('/lembretes', async (req, res) => {
         res.status(500).send(error);
     }
 });
-
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
