1a36bda5d2be0121bf64e3b0849a3b6db3c5d887
Mauro André Ré Júnior <20192PF.CC0170@passofundo.ifsul.local>
2023-06-21 10:19:31 -0300

    Separando a API de dados da autenticação

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -25,16 +23,6 @@ mongoose.connect('mongodb+srv://maurojuniorpf170:dVegIlPx9npPtxUn@tarefas.fhtcpa
 });
 
 
-const userSchema = new mongoose.Schema({
-
-    username: String,
-    password: String
-
-});
-
-const User = mongoose.model('User', userSchema);
-
-
 const lembreteSchema = new mongoose.Schema({
 
     nome: String,
@@ -45,44 +33,9 @@ const lembreteSchema = new mongoose.Schema({
 
 const Lembrete = mongoose.model('lembretes', lembreteSchema);
 
-// Configuração para autenticação com JWT
-const secret = 'auth-api-test';
 
 
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    // Verifica as credenciais
-    const user = await User.findOne({ username });
-
-    if (!user || !bcrypt.compareSync(password, user.password)) {
-        return res.status(401).json({ error: 'Credenciais inválidas' });
-    }
-
-    // Crie o token 
-    const token = jwt.sign({ userId: user._id }, secret, { expiresIn: '1h' });
-
-    res.json({ token });
-});
-
-const authenticateToken = (req, res, next) => {
-    const token = req.headers.authorization;
-
-    if (!token) {
-        return res.status(401).json({ error: 'Token não fornecido' });
-    }
-
-    jwt.verify(token, secret, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ error: 'Token inválido' });
-        }
-
-        req.userId = decoded.userId;
-        next();
-    });
-};
-
-app.get('/lembretes', authenticateToken, async (req, res) => {
+app.get('/lembretes', async (req, res) => {
     try {
         const lembretes = await Lembrete.find();
         if (lembretes.length === 0) {
@@ -95,7 +48,7 @@ app.get('/lembretes', authenticateToken, async (req, res) => {
     }
 });
 
-app.post('/lembretes', authenticateToken, async (req, res) => {
+app.post('/lembretes', async (req, res) => {
     try {
         const lembrete = new Lembrete(req.body);
         await lembrete.save();
@@ -107,7 +60,7 @@ app.post('/lembretes', authenticateToken, async (req, res) => {
 
 
 // atualiza um lembrete existente
-app.put('/lembretes/:id', authenticateToken, async (req, res) => {
+app.put('/lembretes/:id', async (req, res) => {
     try {
         const lembrete = await Lembrete.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.status(200).send({ message: "Lembrete atualizado com sucesso." });
@@ -117,7 +70,7 @@ app.put('/lembretes/:id', authenticateToken, async (req, res) => {
 });
 
 // deleta um lembrete existente
-app.delete('/lembretes/:id', authenticateToken, async (req, res) => {
+app.delete('/lembretes/:id', async (req, res) => {
     try {
         const lembrete = await Lembrete.findByIdAndDelete(req.params.id);
         res.status(200).send('Lembrete deletado com sucesso.');
@@ -127,7 +80,7 @@ app.delete('/lembretes/:id', authenticateToken, async (req, res) => {
 });
 
 // deleta todos os lembretes
-app.delete('/lembretes', authenticateToken, async (req, res) => {
+app.delete('/lembretes', async (req, res) => {
     try {
         await Lembrete.deleteMany();
         res.send('Todos os lembretes foram deletados');
