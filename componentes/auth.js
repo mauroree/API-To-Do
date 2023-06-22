355b9538440b672268bd1eb0d7754a9a4d3be8d0
mauroree <mauroree@gmail.com>
2023-06-21 22:22:17 -0300

    Implementação da API de gateway e de autenticação e ajustes nas rotas

--- componentes/auth.js
+++ componentes/auth.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 
@@ -7,63 +6,67 @@ const app = express();
 const port = 5000;
 app.use(express.json());
 
-// Gera uma chave secreta aleatória
+
 function generateSecretKey() {
   return crypto.randomBytes(32).toString('hex');
 }
 
-// Chave secreta para assinar os tokens JWT
-    const secretKey = generateSecretKey();
+const secretKey = generateSecretKey();
 
-    console.log(secretKey);
+console.log(secretKey);
 
-    const user = {
-    id: 1,
-    username: 'mrxjr',
-    password: '$2b$10$3a2rjkgU9Lg5a5Vc/g3EKecWEq8/nr9S9RfDvihsFvh.HxprMEfO6', 
-    
+const user = {
+  id: 1,
+  username: 'mrxjr',
+  password: '123456987',
+
 };
 
-    // Rota de autenticação
-    app.post('/login', (req, res) => {
-    const { username, password } = req.body;
 
-    // Verificar se o usuário existe e a senha está correta
-    if (username === user.username && bcrypt.compareSync(password, user.password)) {
-        // Gerar token JWT com a identificação do usuário
-        const token = jwt.sign({ id: user.id }, secretKey, { expiresIn: '1h' });
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
 
-        res.json({ token });
-    } else {
-        res.status(401).json({ error: 'Credenciais inválidas' });
+  console.log('Tentativa de login:', { username, password });
+
+  if (username === user.username && password === user.password) {
+ 
+    console.log('Comparação de senha bem-sucedida:', { username });
+
+    const token = jwt.sign({ id: user.id }, secretKey, { expiresIn: '1h' });
+
+    console.log('Login bem-sucedido:', { username });
+
+    res.json({message: 'Login Bem sucedido!', token });
+  } else {
+
+    console.log('Comparação de senha falhou:', { username });
+
+    res.status(401).json({ error: 'Credenciais inválidas' });
+  }
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
     }
-    });
 
-    // Middleware para verificar a autenticação do token JWT
-    function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    req.user = user;
+    next();
+  });
+}
 
-    if (token == null) {
-        return res.sendStatus(401);
-    }
-
-    jwt.verify(token, secretKey, (err, user) => {
-        if (err) {
-        return res.sendStatus(403);
-        }
-
-        req.user = user;
-        next();
-    });
-    }
-
-// Rota protegida que requer autenticação
 app.get('/protegido', authenticateToken, (req, res) => {
   res.json({ message: 'Acesso autorizado!', user: req.user });
 });
 
-// Inicie o servidor da API de autenticação
 app.listen(port, () => {
   console.log(`API de autenticação rodando na porta ${port}`);
 });
--- componentes/gateway.js
+++ componentes/gateway.js
@@ -6,19 +6,63 @@ const app = express();
 const port = 4000;
 app.use(cors());
 
-const apiProxy = createProxyMiddleware('/api', {
-  target: 'http://localhost:3000/lembretes', 
+
+const authProxy = createProxyMiddleware('/login', {
+
+  target: 'http://localhost:5000',
   changeOrigin: true,
+
+});
+
+app.use(authProxy);
+
+const authProtegido = createProxyMiddleware('/protegido', {
+
+  target: 'http://localhost:5000',
+  changeOrigin: true,
+
+});
+
+app.use(authProtegido);
+
+const apiProxy = createProxyMiddleware('/lembretes', {
+  target: 'http://localhost:3000',
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+    // Verificar se o token de autenticação está presente no cabeçalho da requisição
+    const token = req.headers['authorization'];
+
+    if (token) {
+      // Adicionar o token ao cabeçalho da requisição para a API de lembretes
+      proxyReq.setHeader('Authorization', token);
+    } else {
+      // Se o token não estiver presente, negar o acesso
+      res.status(401).send({ error: 'Acesso não autorizado' });
+    }
+  }
 });
 
 app.use(apiProxy);
 
-app.get('/gateway', (req, res) => {
-  res.send('Bem-vindo à API Gateway!');
+const apiIdProxy = createProxyMiddleware('/lembretes/id', {
+  target: 'http://localhost:3000',
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+
+    const token = req.headers['authorization'];
+
+    if (token) {
+
+      proxyReq.setHeader('Authorization', token);
+    } else {
+
+      res.status(401).send({ error: 'Acesso não autorizado' });
+    }
+  }
 });
 
+app.use(apiIdProxy);
 
 app.listen(port, () => {
-  console.log(`API Gateway rodando na porta ${port}`);
+  console.log(`Gateway rodando na porta ${port}`);
 });
-    
